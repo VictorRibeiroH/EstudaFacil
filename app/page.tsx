5380fe622f6359b4b8d18ca46975b842a991eb11
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-10-24 15:20:19 -0300

    Features/CTA

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { features } from "@/constants";
 import Link from "next/link";
 
 export default function Home() {
   return <MaxWidthWrapper>
     <HeroSection />
+    <FeaturesSection />
+    <CTASection />
   </MaxWidthWrapper>;
 }
 
@@ -40,4 +43,46 @@ function HeroSection() {
       </div>
     </div>
   );
+}
+
+function FeaturesSection() {
+  return (
+    <div className="py-20">
+      <h2 className="text-3xl font-bold text-center mb-12">
+        Por que optar pelo nosso organizador de aulas?
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        {
+          features.map((feature, index) => (
+            <Card key={index} className="text-center">
+              <CardHeader>
+                <CardTitle>{feature.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{feature.description}</p>
+              </CardContent>
+            </Card>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+function CTASection () {
+  return (
+    <div className="py-20 text-center">
+      <h2 className="text-3xl font-bold mb-6">
+         Quer mudar completamente a forma como planeja suas aulas?
+      </h2>
+      <p className="text-xl text-muted-foreground mb-10 max-w-2xl mx-auto">
+         Junte-se a centenas de professores que já estão economizando tempo com a criação de planos de aula.
+      </p>
+      <Link href="/pricing"
+        className={buttonVariants()}
+      >
+        Comece agora mesmo
+      </Link>
+    </div>
+  )
 }
---
+++ constants.ts
@@ -0,0 +1,20 @@
+export const features = [
+  {
+    title: "Automação com Inteligência Artificial",
+    description: "Acelere a criação de planos de aula com a ajuda da nossa IA avançada.",
+  },
+  {
+    title: "Templates Flexíveis",
+    description:
+      "Personalize seus planos de aula conforme seu método de ensino.",
+  },
+  {
+    title: "Conformidade com o Currículo",
+    description:
+      "Garanta que seus planos de aula atendam aos padrões e objetivos educacionais.",
+  },
+  {
+    title: "Eficiência no Planejamento",
+    description: "Simplifique seu processo de planejamento para dedicar mais tempo aos seus alunos.",
+  },
+];
